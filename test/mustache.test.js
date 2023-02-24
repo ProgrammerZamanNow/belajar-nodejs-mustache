0225b09f1ad93b42c20e549d818ed78ef086f533
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-02-25 00:14:10 +0700

    Partials

--- test/mustache.test.js
+++ test/mustache.test.js
@@ -148,3 +148,25 @@ test("Comment", async () => {
     expect(data).toContain("Eko");
     expect(data).not.toContain("Ini Komentar");
 });
+
+test("Partials", async () => {
+    const contentTemplate = await fs.readFile("./templates/content.mustache")
+        .then(data => data.toString());
+    const headerTemplate = await fs.readFile("./templates/header.mustache")
+        .then(data => data.toString());
+    const footerTemplate = await fs.readFile("./templates/footer.mustache")
+        .then(data => data.toString());
+
+    const data = Mustache.render(contentTemplate, {
+        title: "Eko",
+        content: "Belajar Mustache JS"
+    }, {
+        header: headerTemplate,
+        footer: footerTemplate
+    });
+
+    console.info(data);
+    expect(data).toContain("Eko");
+    expect(data).toContain("Belajar Mustache JS");
+    expect(data).toContain("Powered by Programmer Zaman Now");
+});
