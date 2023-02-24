dccec49c35926b926d38ce90a63c94fd62714c92
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-02-24 11:00:46 +0700

    Mustache Cache

--- test/mustache.test.js
+++ test/mustache.test.js
@@ -5,3 +5,12 @@ test("Menggunakan Mustache", () => {
     // Hello Eko
     expect(data).toBe("Hello Eko");
 });
+
+
+test("Menggunakan Mustache Cache", () => {
+    Mustache.parse("Hello {{name}}");
+
+    const data = Mustache.render("Hello {{name}}", {name: "Eko"});
+    // Hello Eko
+    expect(data).toBe("Hello Eko");
+});
